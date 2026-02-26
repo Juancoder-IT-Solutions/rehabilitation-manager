028d35a08dd701f2dd7b9e5276719f4ec61a7e56
Jeffred Lim <58154782+jepjepj@users.noreply.github.com>
2026-02-26 16:46:55 +0800

    payment success

---
+++ app/payment-success/page.tsx
@@ -0,0 +1,38 @@
+'use client'
+
+import sampleController from "../controllers/sample_controller"
+
+const About = () => {
+
+    const test = async () => {
+    //    const data = await sampleController.sample_api()
+    //    console.log(data)
+    }
+
+    return (
+        <div className="page-wrapper">
+            <div className="page-header d-print-none">
+                <div className="container-xl">
+                    <div className="row g-2 align-items-center">
+                        <div className="col">
+                            <h2 className="page-title text-green">
+                                Successfull Payment!
+                            </h2>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className="page-body">
+                <div className="container-xl">
+                    <div className="card">
+                        <div className="card-body bg-success text-white">
+                            You can now go back to RehabManager Application.
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default About
